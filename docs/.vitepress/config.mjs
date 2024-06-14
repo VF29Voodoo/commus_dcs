a43e2fe986a8b428763813761bb59afd9ac9b3d6
kerboul <crowbar51kerbal@gmail.com>
2024-06-14 23:25:20 +0000

    feat: Update social link to point to new GitHub repository

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -57,7 +57,7 @@ export default defineConfig({
     ],
 
     socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+      { icon: 'github', link: 'https://github.com/DaKerboul/commus_dcs' }
     ],
 
     search: {
